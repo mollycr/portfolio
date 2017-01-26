c050ecc63c15e41dbdea8802da22d648bf6ccdd9
Molly Meliora <molly@Garryl.local>
2017-01-25 17:05:58 -0800

    rough, non-functional desktop css

--- js/main.js
+++ js/main.js
@@ -3,29 +3,34 @@ angular.module('portfolio', [])
     var app = this;
 
     app.labels = {
+      tagline: "Connecting design and code",
       name: "Molly Robison",
-      headline: "UI/UX Designer",
       work: "Work",
       resume: "Resume",
       about: "About Me",
       contact: "Contact"
     };
 
+    app.text = {
+      about: ["I believe that design is a social issue. We wield a lot of power as information professionals - we decide what to highlight and what to hide, and influence how people feel. As our society becomes more intertwined with technology, the design decisions that we make have a stronger impact on humanity. We can use design to empower, or to disenfranchise, and we should not take that power lightly. The internet is our third space, our commons, and we need to be mindful that we&apos;re not just building products, we&apos;re building a world. I work to make that world liveable.",
+       "In my spare time I officiate with the Peninsula Roller Girls as Joelle Van Die, sew pretty poorly, and knit pretty well."],
+    };
+
     app.projects = [
       {title: "Appspirin",
-        img: "img/thumbs/appspirin.jpg"
+        img: "img/thumbs/appspirin.png"
       },
       {title: "Teledraw",
-        img: "img/thumbs/teledraw.jpg"
+        img: "img/thumbs/teledraw.png"
       },
       {title: "Auction Pal",
-        img: "img/thumbs/auction.jpg"
+        img: "img/thumbs/auctionPal.png"
       },
       {title: "Foreclosure",
-        img: "img/thumbs/foreclosure.jpg"
+        img: "img/thumbs/foreclosure.png"
       },
       {title: "Art",
-        img: "img/thumbs/art.jpg"
+        img: "img/thumbs/utensils.png"
       }
     ];
   });
